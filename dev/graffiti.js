f0030a8da4cb27727eea8fcd50b38bddcbda5998
admin <admin@44bz.com>
2017-03-13 20:36:35 +0800

    取消弹出窗口。改为在当前窗口中加入聚合按钮，点击聚合显示，则聚合图片，点击聚合隐藏则隐藏聚合图片。

--- dev/graffiti.js
+++ dev/graffiti.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         涂鸦by SeLang
 // @namespace    http://cmsv1.findmd5.com/
-// @version      0.1
-// @description  涂鸦之作，节约浏览网页的时间成本。目前针对蕾丝猫里详情页的美女图片进行了聚合，不用手动翻页。QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea
+// @version      0.2
+// @description  涂鸦之作，节约浏览网页的时间成本。目前针对蕾丝猫里详情页的美女图片进行了聚合，不用手动翻页。
+// @description  QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea
 // @author       selang
 // @include       /https?\:\/\/www\.lesmao\.com/
 // @require       https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js
 // @connect      *
 // @grant        GM_download
+// @grant        GM_openInTab
+// @grant        GM_getTab
+// @grant        GM_getTabs
+// @grant        GM_saveTab
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
@@ -21,42 +26,71 @@
         var preUrl = match[1];
         var currentPageNum = match[2];
         var subfixUrl = match[3];
-        console.log(preUrl + currentPageNum + subfixUrl);
-
-        var myWindow = window.open('', '', 'width=300,height=600');
-        injectJs(myWindow);
-        callBackJs(myWindow, preUrl, subfixUrl);
-        myWindow.focus();
-    } else {
+        currentWindowImpl(preUrl, subfixUrl);
+    }
+    else {
         // Match attempt failed
     }
+
+
 })();
+
+function currentWindowImpl(preUrl, subfixUrl) {
+    dependenceJQuery(window, injectBtn(window,function (e) {
+        if($('#injectaggregatBtn').val()==='聚合显示'){
+            $('#injectaggregatBtn').val('聚合隐藏');
+            collectPics(window, preUrl, subfixUrl);
+            $('#c_container').show();
+        }else {
+            $('#injectaggregatBtn').val('聚合显示');
+            $('#c_container').hide();
+        }
+    }));
+}
+
+
+function log(c) {
+    console.log(c);
+}
 
 //注入JS：jquery
 function injectJs(e) {
-    var ele = e.document.createElement('script');
-    ele.src = "https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js";
-    e.document.body.appendChild(ele);
+    if (e.jQuery) {
+        log('yes');
+    } else {
+        var ele = e.document.createElement('script');
+        ele.src = "https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js";
+        e.document.body.appendChild(ele);
+        var id = e.setInterval(function () {
+            if (e.jQuery) {
+                e.clearInterval(id);
+            }
+        }, 100);
+    }
+}
+
+function dependenceJQuery(e, callback) {
     var id = e.setInterval(function () {
-        if (typeof e.$ != 'undefined') {
+        if (e.jQuery) {
             e.clearInterval(id);
+            callback;
         }
     }, 100);
 }
 
 //JS注入成功后，回调
-function callBackJs(e, preUrl, subfixUrl) {
+function collectPics(e, preUrl, subfixUrl) {
     var id = e.setInterval(function () {
-        if (typeof e.$ != 'undefined') {
+        if (e.$) {
             e.clearInterval(id);
             var breakPageLoop = false;
             for (var i = 1; i < 30; i++) {
                 //创建div去装各自
-                e.$('body').append('<div id="c_' + i + '"></div>');
+                e.$('#c_container').append('<div id="c_' + i + '"></div>');
                 if (!breakPageLoop) {
                     var lock = true;
                     obtainHtml(preUrl + i + subfixUrl, function (html, i) {
-                        //console.log(html);
+                        //log(html);
                         var parser = new DOMParser();
                         var doc = parser.parseFromString(html, "text/html");
                         var status = query(e.$('#c_' + i), $(doc).find('ul > li > img'));
@@ -65,7 +99,8 @@ function callBackJs(e, preUrl, subfixUrl) {
                         }
                         lock = false;
                     }, i);
-                } else {
+                }
+                else {
                     break;
                 }
             }
@@ -76,7 +111,7 @@ function callBackJs(e, preUrl, subfixUrl) {
 //查询图片
 function query(objContainer, jqObj) {
     jqObj.each(function (index) {
-        console.log(index + ": " + $(this).prop('outerHTML'));
+        //log(index + ": " + $(this).prop('outerHTML'));
         var imgSrc = $(this).attr('src');
         if ('http://i.ajjc.net/k/1178/' === imgSrc) {
             return 'end page';
@@ -88,12 +123,21 @@ function query(objContainer, jqObj) {
 //获取网页
 function obtainHtml(url, sucess, i) {
     //GM_download('http://www.w3school.com.cn/jquery/test1.txt', "就好");
-   GM_xmlhttpRequest({
+    GM_xmlhttpRequest({
         method: 'GET',
-        headers: {"Accept": "application/*"},
+        headers: {
+            "Accept": "application/*"
+        },
         url: url,
         onload: function (response) {
             sucess(response.responseText, i);
         }
     });
+}
+
+
+function injectBtn(e,callback) {
+    $('.thread-tr').after('<input type="button" id="injectaggregatBtn" value="聚合显示"/>');
+    $('#injectaggregatBtn').after('<div id="c_container"></div>');
+    $('#injectaggregatBtn').bind('click',callback);
 }
