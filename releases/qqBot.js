8dee5a5c9cf566201e44999b24ba73e4df6c2d56
admin <admin@44bz.com>
2017-03-14 19:40:31 +0800

    补齐图灵的其他种类

--- releases/qqBot.js
+++ releases/qqBot.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QQ机器人by SeLang
 // @namespace    http://cmsv1.findmd5.com/
-// @version      0.3
+// @version      0.4
 // @description  目标是实现一些人性化的功能，有想法的请反馈。注意：纯图片视频等不捕捉；表情会被转成超链接；含图片仅回复文字;匿名聊天不捕捉。 QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea。
 // @author       selang
 // @include       /https?\:\/\/w\.qq\.com/
@@ -16,11 +16,11 @@
 // ==/UserScript==
 
 //图灵机器人key,改这里
-var tulingKey='64b194e6e27740fcbee72869d1b8ec81';
+var tulingKey = '64b194e6e27740fcbee72869d1b8ec81';
 
 (function () {
     'use strict';
-    priorityLog('看到这里，你肯定是个老司机了。欢迎老司机进群：455809302交流。一起玩。\r\n如果不是老司机，只要有创意也欢迎加入。加群双击，暴击:https://jq.qq.com/?_wv=1027&k=45p9bea。');
+    priorityLog('看到这里，你肯定是个老司机了。欢迎老司机进群：455809302交流。一起玩。\r\n如果不是老司机，只要有创意也欢迎加入。点击链接加入群【油猴脚本私人级别定制】：https://jq.qq.com/?_wv=1027&k=460soLy');
     priorityLog('已加入图灵机器人API，请填入自己的图灵机器人key。key申请：http://www.tuling123.com/');
     priorityLog('参考海绵宝宝的想法，加入图灵机器人API，一肚子坏水会不会来打我O(∩_∩)O');
     priorityLog('测试QQ群号：550738206,更改默认群，欢迎进群炸机器人,点击链接加入群【web qq机器人测试群】：https://jq.qq.com/?_wv=1027&k=460shtL');
@@ -59,18 +59,32 @@ function listenChatContent(target) {
             var chat_content = $(otherPersons[len - 1]).find('.chat_content').html();
             var chat_nick = $(otherPersons[len - 1]).find('.chat_nick').html();
             // window.clearInterval(id);
-            if(chat_content.startsWith('#')){
+            if (chat_content.startsWith('#')) {
                 //图灵API对接
                 obtainHtml_POST('http://www.tuling123.com/openapi/api', chat_content, qqNum, function (txt) {
-                    var data=JSON.parse(txt);
+                    var data = JSON.parse(txt);
                     log(data);
-                    var msg=data.text;
-                    if(data.url){
-                        msg+='\r\n'+data.url;
+                    var msg = data.text;
+                    if (data.url) {
+                        msg += '\r\n' + data.url;
+                    }
+                    if (data.list) {
+                        for (o in data.list) {
+                            if (data.list[o].article) {
+                                if (data.list[o].article === '') {
+                                    msg += '\r\n' + data.list[o].source + '\r\n' + data.list[o].detailurl;
+                                } else {
+                                    msg += '\r\n' + data.list[o].article + '\r\n' + data.list[o].detailurl;
+                                }
+                            }
+                            if (data.list[o].name) {
+                                msg += '\r\n' + data.list[o].name + '\r\n' + data.list[o].info + '\r\n' + data.list[o].detailurl;
+                            }
+                        }
                     }
                     sendMsg(msg);
                 });
-            }else {
+            } else {
                 //sendMsg(chat_nick + '_' + qqNum + '你刚刚说了：' + '\r\n' + chat_content);
             }
         } else {
