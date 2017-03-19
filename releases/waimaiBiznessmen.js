fea74c4061d7d3ade6ce2cb9913d54f1ae2c7eeb
selang <junlin.li.cn@gmail.com>
2017-03-19 11:09:56 +0000

    enhance waimaiBiz

--- releases/waimaiBiznessmen.js
+++ releases/waimaiBiznessmen.js
@@ -6,6 +6,7 @@
 // @author       selang
 // @include       /https?\:\/\/waimai\.meituan\.com/
 // @require       https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js
+// @require       https://raw.githubusercontent.com/dchester/jsonpath/0.2.10/jsonpath.min.js
 // @connect      *
 // @grant        GM_download
 // @grant        GM_openInTab
@@ -20,8 +21,9 @@ var tulingKey = '64b194e6e27740fcbee72869d1b8ec81';
 
 (function() {
     'use strict';
+    ///geo/geohash?lat=30.796654&lng=106.09299&addr=%25E6%2596%2587%25E5%258C%2596%25E8%25B7%25AF&from=m
     priorityLog('看到这里，你肯定是个老司机了。欢迎老司机进群：455809302交流。一起玩。\r\n如果不是老司机，只要有创意也欢迎加入。点击链接加入群【油猴脚本私人级别定制】：https://jq.qq.com/?_wv=1027&k=460soLy');
-    collectoerResult('https://waimai.meituan.com/ajax/poilist?_token=eJx9kV9v0zAUxb+LH3iptdiO42tXmtBQWRfGgI21Kp0QSposyVL3T5qQtojvzrU7Kp6IIuWnc4/Pvbn+RZo4I0PO8AFK2h2y1EYbASqEUFCy+EfTzEgwlKTNdESGT5zJiAKw7055QOGJK8aoYaicUCtEIfF1nhgtpGzbzTAI+qSySXVh86rtktXFYm2Dcm3zoLfQrFbb3KRQEvof99uutT8Wid0kVbG6TJMq6944abfumkV+ySMhCDa1j66p5pxyEAoT8R8QQ+ZROjQenUE6BKMQI+2RIYJDHirHyrPkkXNzz+BCmIvGnnhUQehZoUdF0rMWyNJ7QoGdQo/CaMpF5BHQzX24kDgg99nCjc3cqFgT1IAnof8Slk/EjD4TXoDPYRqrfhQGIdUnTXEKvjeLOFXGE96yOmk42ivxs8a8DzdZu03iNzlvVAMdxVNXa19rO7wLMiT5h/2nqn5/W4yu7vov6tBreBwYeTh05XidHK+ndjyY1ZP7lC3j7dXe8nG6386L5vO7efFSrLMKHqy6WWazl4m6S/RG1d+m4dFc15oF4XLyvJndxD+f+9uvh+y4bXRdiGUxmRdRed8ORsePNfn9B60zt6s=')
+    collectoerResult('https://waimai.meituan.com/ajax/poilist');
 })();
 
 function collectoerResult(url) {
@@ -35,11 +37,35 @@ function collectoerResult(url) {
         page_offset: '21',
         page_size: '20'
     };
+    url+='?_token='+Rohr_Opt.reload(urlFormObj);
+    log(url);
     obtainHtml_POST(url,
         urlEncode(urlFormObj),
         function(responseText) {
-            log(responseText);
+            var json=JSON.parse(responseText);
+            log(json);
+            var findObj=jsonpath.query(json,'$..wmPoi4Web');
+            var filterArr=['name','address','call_center','latitude','longitude','month_sale_num'];
+            findObj.forEach( function(element, index) {
+            //     log(element.filter(entry){
+            //         log('-->'+entry);
+            // });
         });
+}
+
+function deepFind(obj, path) {
+  var paths = path.split('.')
+    , current = obj
+    , i;
+
+  for (i = 0; i < paths.length; ++i) {
+    if (current[paths[i]] == undefined) {
+      return undefined;
+    } else {
+      current = current[paths[i]];
+    }
+  }
+  return current;
 }
 
 //获取参数
