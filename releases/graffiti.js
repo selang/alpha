2b2fe5b0da6460d1ef6284beea38a2584e941920
admin <admin@44bz.com>
2017-03-23 18:33:38 +0800

    移除umei.cc图片附属广告；移除meitulu.com图片广告遮挡层

--- releases/graffiti.js
+++ releases/graffiti.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         美女图聚合展示by SeLang
 // @namespace    http://cmsv1.findmd5.com/
-// @version      1.3
+// @version      1.4
 // @description  目标是聚合美女图片，省去翻页烦恼。已实现：蕾丝猫(lesmao.com)，优美(umei.cc)，美图录(meitulu.com)，美女86(17786.com)。待实现：。有需要聚合的网址请反馈。 QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea
 // @author       selang
 // @include       /https?\:\/\/www\.lesmao\.com/
@@ -216,9 +216,17 @@ function collectPics(e, preUrl, limitPage, subfixUrl, currentHostname) {
                             imgObj = $(doc).find('ul > li > img');
                         } else if ('www.umei.cc' === currentHostname) {
                             imgObj = $(doc).find('.ImageBody p img');
+                            {//移除图片附属广告
+                                $('div div div.ad-widget-imageplus-sticker').parent().parent().remove();
+                            }
                         }
                         else if ('www.meitulu.com' === currentHostname) {
                             imgObj = $(doc).find('div.content > center  > img');
+                            {//http://www.meitulu.com广告遮挡层
+                                $("a[id^='__tg_ciw_a__']").remove();
+                                $("a[id^='__qdd_ciw_a__']").remove();
+                                $('iframe').remove();//移除广告等无必要元素
+                            }
                         } else if ('www.17786.com' === currentHostname) {
                             imgObj = $(doc).find('img.IMG_show');
                         }
