42bbee7db7b824354f28fc03fd9aa26e6f923d0c
admin <admin@44bz.com>
2017-03-27 18:45:39 +0800

    基于自输入种子地址来获取内容

--- releases/p2pshare.js
+++ releases/p2pshare.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         P2P分享by SeLang
 // @namespace    http://cmsv1.findmd5.com/
-// @version      0.1
+// @version      0.2
 // @description  目标是网页右键收藏，一键打包种子，一键分享，浏览器直接查看，在线编辑发布网站。 QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea。
 // @author       selang
 // @include       /https?\:\/\/help\.baidu\.com/
 // @require       https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js
 // @require       https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js
+// @require       https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.1/angular.min.js
 // @connect      *
 // @grant        GM_download
 // @grant        GM_openInTab
@@ -22,25 +23,28 @@
 // ==/UserScript==
 
 var imgType = ['.jpg', '.gif', '.jpeg'];
-const foreSuffix = '.selang';
+const forceSuffix = '.selang';
 
 (function () {
     'use strict';
     priorityLog('欢迎进群：455809302交流。一起玩。\r\n如果不是老司机，只要有创意也欢迎加入。点击链接加入群【油猴脚本私人级别定制】：https://jq.qq.com/?_wv=1027&k=460soLy');
     priorityLog('约定内容后缀名名为.selang');
-    clearHeadAndBody();
+    priorityLog('种子分享网站：(https://btorrent.xyz/)，建立一个文本文档以.selang结尾；文档里每一行放入一个图片的链接地址，再在网站上分享。拷贝种子或者磁力链接地址到对应页面，点击获取，一会应该就能获取到种子里的图片了');
+    priorityLog('例子：建立一个文本文档以.selang结尾；文档里每一行放入一个图片的链接地址，再在网站上分享。拷贝种子或者磁力链接地址到(http://help.baidu.com)，点击获取，一会应该就能获取到种子里的图片了');
+    injectTorrentInputComponent();
+})();
+
+function torrentParse(torrentVal) {
     if (WebTorrent.WEBRTC_SUPPORT) {
         var client = new WebTorrent();
-        var torrentId = 'magnet:?xt=urn:btih:7cdfd7105540b2eb0255ca72cd640e25a59e8cef&dn=test.selang&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com';
+        var torrentId = torrentVal;
         client.add(torrentId, function (torrent) {
-            clearHeadAndBody();
-
             var length = torrent.files.length;
             log('共有文件：' + length);
             forEach(torrent.files, function (file) {
                 log(file);
                 var path = file.path;
-                if (foreSuffix === getSuffix(path)) {
+                if (forceSuffix === getSuffix(path)) {
                     file.getBuffer(function (err, buffer) {
                         if (err) throw err;
                         var text = buffer.toString();
@@ -49,7 +53,7 @@ const foreSuffix = '.selang';
                         forEach(txtArray, function (txt) {
                             var type = getSuffix(txt);
                             if (imgType.indexOf(type) != -1) {
-                                $('body').append('<img src=' + txt + ' alt=""/>');
+                                $('#mainContainer').append('<img src=' + txt + ' alt=""/>');
                             }
                         });
                     });
@@ -59,8 +63,21 @@ const foreSuffix = '.selang';
     } else {
         priorityLog('不支持！')
     }
+}
 
-})();
+function injectTorrentInputComponent() {
+    clearHeadAndBody();
+    $('body').append('<input id="torrentVal" type="text" value="" placeholder="请输入特制的种子地址"/><input id="torrentBtn" type="button" value="获取"/>');
+    bindBtn(window,function (e) {
+        var torrentVal = $('#torrentVal').val();
+        torrentParse(torrentVal);
+    });
+    $('body').append('<div id="mainContainer"></div>');
+}
+
+function bindBtn(e, callback) {
+    $('#torrentBtn').bind('click', callback);
+}
 
 function clearHeadAndBody() {
     $('body').html('');
@@ -94,7 +111,7 @@ function getUrlParam(name) {
 
 //日志
 function log(c) {
-    if (true) {
+    if (false) {
         console.log(c);
     }
 }
