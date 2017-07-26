1febbfbe329fcb9a73371854cc6255e6e39b7760
admin <admin@44bz.com>
2017-07-26 12:16:52 +0800

    最低价按钮，自动填写无脑内容

---
+++ releases/market.user.js
@@ -0,0 +1,203 @@
+// ==UserScript==
+// @name         IG-XE-CS-GO
+// @namespace    http://cmsv1.findmd5.com
+// @version      0.0.1
+// @description
+// @author       clownfish
+// @include      /https?:\/\/www\.igxe\.cn/
+// @run-at       document-end
+// ==/UserScript==
+var igxecsgo_Name = 'IG-XE-CS-GO';
+var buyer_name = jQuery('#js-show-user-pane > a.s.vam.omit.dib').text();
+var putwayPWD = '你的上架密码';
+var payPWD = '你的支付密码';
+if (buyer_name) {
+    sessionStorage.setItem('buyer_name', buyer_name);
+}
+
+function igxecsgo_ChechItemPriceRatio() {//列表页方法
+    var igxecsgo_ItemPrice = jQuery("div.s2 span b");//市场价
+    var igxecsgo_ItemPrice2 = jQuery("div.s3 span strong");//卖家价
+    var igxecsgo_ItemCount2 = 1;
+    igxecsgo_ItemPrice2.each(function () {
+        var _ratio = (Number(igxecsgo_ItemPrice2[igxecsgo_ItemCount2 - 1].innerText) / Number(igxecsgo_ItemPrice[igxecsgo_ItemCount2 - 1].innerText)).toFixed(4);
+        if (_ratio <= 0.5) {
+            jQuery(this).parent().parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#fff;\">' + _ratio + '</strong> ->先下手为强</p>');
+            jQuery(this).parent().parent().parent().parent().css({"border": "2px solid #fff"});
+            layer.open({title: igxecsgo_Name, content: '本页有低于0.5比例的饰品!'});
+        } else if (0.5 < _ratio && _ratio <= 0.6) {
+            jQuery(this).parent().parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#09f;\">' + _ratio + '</strong> ->好价速秒</p>');
+            jQuery(this).parent().parent().parent().parent().css({"border": "2px solid #09f"});
+        } else if (0.6 < _ratio && _ratio <= 0.7) {
+            jQuery(this).parent().parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#080;\">' + _ratio + '</strong> ->大商出货</p>');
+            jQuery(this).parent().parent().parent().parent().css({"border": "2px solid #080"});
+        } else if (0.7 < _ratio && _ratio <= 0.8) {
+            jQuery(this).parent().parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#a0ff58;\">' + _ratio + '</strong> ->正常贸易</p>');
+            jQuery(this).parent().parent().parent().parent().css({"border": "2px solid #a0ff58"});
+        } else if (0.8 < _ratio && _ratio <= 1) {
+            jQuery(this).parent().parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#f0ad4e;\">' + _ratio + '</strong> ->建议去Steam市场</p>');
+            jQuery(this).parent().parent().parent().parent().css({"border": "2px solid #f0ad4e"});
+            // jQuery(this).parent().parent().parent().parent().remove();
+        } else {
+            jQuery(this).parent().parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#c43f0f;\">' + _ratio + '</strong> ->玄学,静待有缘人</p>');
+            jQuery(this).parent().parent().parent().parent().css({"border": "2px solid #c43f0f"});
+            // jQuery(this).parent().parent().parent().parent().remove();
+        }
+        igxecsgo_ItemCount2 += 1;
+    });
+    igxecsgo_ItemPrice.parent().attr('title', '非Steam市场均价!');
+
+    //迅速标记
+    {
+        var salePriceStr = jQuery(jQuery('.s3 strong')[0]).text();
+        var miniSalePrice = parseFloat(salePriceStr);
+        var match = window.location.href.match(/#([^#]+)$/im);
+        if (match != null) {
+            var suffixLogic = match[1];
+            var salePriceStr = getParam(suffixLogic, 'salePrice');
+            if (salePriceStr != null) {
+                var salePrice = parseFloat(salePriceStr);
+                if (miniSalePrice >= salePrice) {
+                    jQuery('ul.widget-filter > li:nth-child(1)').prepend('<strong style=\"color:#c43f0f;\">可赚：' + (miniSalePrice - salePrice).toFixed(2) + '</strong>');
+                }
+            }
+        } else {
+            // Match attempt failed
+        }
+    }
+}
+
+//获取参数
+function getParam(dest, name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = dest.match(reg);
+    if (r != null) return decodeURI(r[2]);
+    return null;
+}
+
+function igxecsgo_ChechItemPriceRatio2() {//详情页方法
+    var igxecsgo_ItemPrice = Number(jQuery("div.s2 span b").text().replace('￥', '').trim());//市场价
+    var igxecsgo_ItemPrice2 = Number(jQuery("div.s3 span strong").text().replace('￥', '').trim());//卖家价
+    var _ratio = (igxecsgo_ItemPrice2 / igxecsgo_ItemPrice).toFixed(4);
+    if (_ratio <= 0.5) {
+        jQuery("div.s3 span strong").parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#fff;\">' + _ratio + '</strong> ->先下手为强</p>');
+        layer.open({title: igxecsgo_Name, content: '本页有低于0.5比例的饰品!'});
+    } else if (0.5 < _ratio && _ratio <= 0.6) {
+        jQuery("div.s3 span strong").parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#09f;\">' + _ratio + '</strong> ->好价速秒</p>');
+    } else if (0.6 < _ratio && _ratio <= 0.7) {
+        jQuery("div.s3 span strong").parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#080;\">' + _ratio + '</strong> ->大商出货</p>');
+    } else if (0.7 < _ratio && _ratio <= 0.8) {
+        jQuery("div.s3 span strong").parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#a0ff58;\">' + _ratio + '</strong> ->正常贸易</p>');
+    } else if (0.8 < _ratio && _ratio <= 1) {
+        jQuery("div.s3 span strong").parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#f0ad4e;\">' + _ratio + '</strong> ->建议去Steam市场看看</p>');
+    } else {
+        jQuery("div.s3 span strong").parent().after('<p style=\"font-size:14px;\">比例: <strong style=\"color:#c43f0f;\">' + _ratio + '</strong> ->玄学,静待有缘人</p>');
+    }
+
+    {
+        var sName = jQuery('.mod-equipmentDetail-bd h3').text().trim();
+        var salePrice = jQuery('.mod-equipmentDetail-bd .s3 span strong').text().trim().replace("￥", "");
+        var exteriorIndex = jQuery('.s1 span').text().trim().replace("外观: ", "");
+        var rarityIndex = jQuery('.s1 b').text().trim();
+        var suffixLogic = '#salePrice=' + salePrice;
+        //加入列表查询按钮
+        jQuery('.s4.js-add-cart-parent').append('<a href="https://www.igxe.cn/csgo/search/0_0?keyword=' +
+            encodeURI(sName) +
+            '&search_page_no=1&search_relate_price=&search_is_sticker=0&search_price_gte=&search_price_lte=&search_rarity_id=' +
+            rarity(rarityIndex) +
+            '&search_exterior_id=' +
+            exterior(exteriorIndex) +
+            '&search_is_stattrak=0&search_sort_key=2&search_sort_rule=0' +
+            suffixLogic +
+            '" target="_blank" class="com-btn com-red" style="overflow: visible;margin-bottom: 20px;" rel="nofollow">查看最低价</a>');
+    }
+
+}
+
+var rarityArray = [];
+rarityArray['588'] = '军规级';
+rarityArray['600'] = '消费级';
+rarityArray['608'] = '受限';
+rarityArray['621'] = '隐秘';
+rarityArray['627'] = '保密';
+rarityArray['654'] = '非凡';
+rarityArray['666'] = '工业级';
+rarityArray['750'] = '违禁';
+rarityArray['军规级'] = '588';
+rarityArray['消费级'] = '600';
+rarityArray['受限'] = '608';
+rarityArray['隐秘'] = '621';
+rarityArray['保密'] = '627';
+rarityArray['非凡'] = '654';
+rarityArray['工业级'] = '666';
+rarityArray['违禁'] = '750';
+
+function rarity(index) {
+    return rarityArray[index];
+}
+
+var exteriorArray = [];
+exteriorArray['589'] = '崭新出厂';
+exteriorArray['595'] = '破损不堪';
+exteriorArray['601'] = '久经沙场';
+exteriorArray['609'] = '战痕累累';
+exteriorArray['615'] = '略有磨损';
+exteriorArray['728'] = '无涂装';
+
+exteriorArray['崭新出厂'] = '589';
+exteriorArray['破损不堪'] = '595';
+exteriorArray['久经沙场'] = '601';
+exteriorArray['战痕累累'] = '609';
+exteriorArray['略有磨损'] = '615';
+exteriorArray['无涂装'] = '728';
+
+function exterior(index) {
+    return exteriorArray[index];
+}
+
+function igxecsgo_SetOrder() {//订单页方法
+    document.getElementById('buyer_name').value = (sessionStorage.getItem('buyer_name') === null) ? '' : sessionStorage.getItem('buyer_name');
+    document.getElementById('pay-pwd').value = payPWD;
+    layer.msg('已自动填写购买者名称', {offset: ['24px', '24px'], shift: 4});
+}
+
+function igxecsgo_SetOn() {//上架
+    setInterval(function () {
+        var spwd = document.getElementById('sale-trade-pwd');
+        if (spwd) {
+            if (spwd.value != putwayPWD) {
+                spwd.value = putwayPWD;
+            }
+        }
+    }, 100);
+}
+
+function igxecsgo_SetOn2() {//上架
+    setInterval(function () {
+        var spwd = document.getElementById('password');
+        if (spwd) {
+            if (spwd.value != putwayPWD) {
+                spwd.value = putwayPWD;
+            }
+        }
+    }, 100);
+}
+
+(function () {
+    if (location.pathname.indexOf('/category') > -1 || location.pathname.indexOf('/productlist') > -1 || location.pathname.indexOf('/shop') > -1 || location.pathname.indexOf('/search') > -1) {
+        igxecsgo_ChechItemPriceRatio();
+    }
+    else if (location.pathname.indexOf('/product') > -1 && location.pathname.indexOf('/productlist') < 0) {
+        igxecsgo_ChechItemPriceRatio2();
+    }
+    else if (location.pathname.indexOf('/payment_order') > -1) {
+        igxecsgo_SetOrder();
+    } else if (location.pathname.indexOf('/inventory/igxe/') > -1) {
+        igxecsgo_SetOn();
+    } else if (location.pathname.indexOf('/purchase/sell_') > -1) {
+        igxecsgo_SetOn2();
+    }
+    else {
+        layer.msg(igxecsgo_Name + '已载入', {offset: ['24px', '24px'], shift: 4});
+    }
+})();
