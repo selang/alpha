c5ca318aeecbddc146ee93b50c24a77f5f1b51ee
admin <admin@44bz.com>
2017-11-10 16:59:46 +0800

    修复蕾丝猫域名变换导致脚本不可用的BUG

--- releases/graffiti.js
+++ releases/graffiti.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         美女图聚合展示by SeLang
 // @namespace    http://cmsv1.findmd5.com/
-// @version      2.9
-// @description  目标是聚合美女图片，省去翻页烦恼。已实现：蕾丝猫(lesmao.com)，优美(umei.cc)，美图录(meitulu.com)，美女86(17786.com)，24美女图片(24meinv.me)。待实现：。有需要聚合的网址请反馈。 QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea
+// @version      2.10
+// @description  目标是聚合网页美女图片，省去翻页烦恼。有需要聚合的网址请反馈。 QQ群号：455809302,点击链接加入群【油猴脚本私人定制】：https://jq.qq.com/?_wv=1027&k=45p9bea
 // @author       selang
-// @include       /https?\:\/\/www\.lesmao\.com/
+// @include       /https?\:\/\/www\.lsmpic\.com/
 // @include       /https?\:\/\/www\.umei\.cc/
 // @include       /https?\:\/\log/www\.meitulu\.com/
 // @include       /https?\:\/\/www\.17786\.com/
@@ -19,6 +19,7 @@
 // @require       https://cdnjs.cloudflare.com/ajax/libs/dom-to-image/2.5.2/dom-to-image.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/dexie/1.5.1/dexie.min.js
+// @require       https://cdnjs.cloudflare.com/ajax/libs/webtorrent/0.98.19/webtorrent.min.js
 // @connect      *
 // @grant        GM_download
 // @grant        GM_openInTab
@@ -27,6 +28,7 @@
 // @grant        GM_saveTab
 // @grant        GM_xmlhttpRequest
 // @grant        GM_registerMenuCommand
+// @grant        unsafeWindow
 // ==/UserScript==
 
 var blobCache = {};
@@ -66,7 +68,7 @@ var Alpha_Script = {
     'use strict';
 
     priorityLog('看到这里，你肯定是个老司机了。欢迎老司机进群：455809302交流。一起玩。\r\n如果不是老司机，只要有创意也欢迎加入。点击链接加入群【油猴脚本私人级别定制】：https://jq.qq.com/?_wv=1027&k=460soLy。');
-    priorityLog('已实现：蕾丝猫(http://www.lesmao.com)，优美(http://www.umei.cc)，美图录(http://www.meitulu.com)，美女86(http://www.17786.com)，宅男女神(http://www.nvshens.com)，24美女图片(http://www.24meinv.me)，爱套图(http://www.aitaotu.com)，妹子图(http://www.mzitu.com)');
+    priorityLog('已实现：蕾丝猫(http://www.lsmpic.com)，优美(http://www.umei.cc)，美图录(http://www.meitulu.com)，美女86(http://www.17786.com)，宅男女神(http://www.nvshens.com)，24美女图片(http://www.24meinv.me)，爱套图(http://www.aitaotu.com)，妹子图(http://www.mzitu.com)');
     priorityLog('已实现：绕过YouTube的年龄限制');
     priorityLog('未实现：');
 
@@ -111,7 +113,7 @@ var Alpha_Script = {
     var commonObj = pagesCommonObj();
     commonObj.meet(
         {
-            domain: 'www.lesmao.com',
+            domain: 'www.lsmpic.com',
             startUrl: currentProtocol + '//' + currentHostname + '/',
             limitPage: 30,
             success: function () {
@@ -388,7 +390,7 @@ function switchVId(vId) {
     if (text && text.indexOf('内容警告') != -1) {
         log('内容警告::');
         $('#player-api').removeClass('off-screen-target').html('<iframe src="https://www.youtube.com/embed/' +
-            vId +
+        vId +
             '" width="100%" height="100%" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>');
         return true;
     }
@@ -474,7 +476,7 @@ function switchAggregationBtn(preUrl, startIndex, limitPage, suffixUrl, currentH
         $('#c_container').show();
 
         var hideObj = {
-            'www.lesmao.com': function () {
+            'www.lsmpic.com': function () {
                 $('#thread-pic').hide();
                 $('#thread-page').hide();
             },
@@ -517,7 +519,7 @@ function switchAggregationBtn(preUrl, startIndex, limitPage, suffixUrl, currentH
         $('#c_container').hide();
 
         var showObj = {
-            'www.lesmao.com': function () {
+            'www.lsmpic.com': function () {
                 $('#thread-pic').show();
                 $('#thread-page').show();
             },
@@ -627,7 +629,7 @@ function collectPics(startIndex, preUrl, limitPage, suffixUrl, currentHostname) 
                         onload: function () {
                             var _i = i;
                             var parseObj = {
-                                'www.lesmao.com': function (doc) {
+                                'www.lsmpic.com': function (doc) {
                                     return $(doc).find('ul > li > img');
                                 },
                                 'www.umei.cc': function (doc) {
@@ -728,7 +730,7 @@ function injectAggregationRef(currentHostname) {
         '<span>&nbsp;&nbsp;</span>' +
         '<input id="injectaggregatBtn" type="button" value="聚合显示"/>';
     var injectAggregateObj = {
-        'www.lesmao.com': function () {
+        'www.lsmpic.com': function () {
             $('.thread-tr').after(injectComponent);
             $('#vt').append(injectComponent);
         },
