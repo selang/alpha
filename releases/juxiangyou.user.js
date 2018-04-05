cc4f3554994740cc1930d622554680bf929b9b3a
admin <admin@44bz.com>
2018-04-05 17:26:53 +0800

    强化

--- releases/juxiangyou.user.js
+++ releases/juxiangyou.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         聚享游豪华套装
 // @namespace    http://cmsv1.findmd5.com/
-// @version      0.1
+// @version      0.2
 // @description  聚享游
 // @author       david
 // @include       /https?\:\/\/www\.juxiangyou\.com/
@@ -26,10 +26,10 @@
     'use strict';
     priorityLog('');
     var pathname = window.location.pathname;
-    if('/fun/play/crazy28/wdjc'===pathname) {
+    // if('/fun/play/crazy28/wdjc'===pathname) {
         $('p.login-p.login-p-id.mar-t4').text('ID：11696158');
         $('span.J_udou').text('123456');
-    }
+    // }
 })();
 
 //日志
